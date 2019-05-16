3eea2b6b37796a2d64a1aa2f52c3ea5f1582570a
Sean Baskin <seanbaskin@gmail.com>
2019-05-16 09:54:09 -0400

    Javadoc comments and refactoring

--- src/main/java/jitter/config/Config.java
+++ src/main/java/jitter/config/Config.java
@@ -1,32 +1,37 @@
 package jitter.config;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
- * 
+ * Represents the YAML configuration file provided by the user.
  */
 public class Config {
     
+    /**
+     * Collection of {@code String} instances representing the path a git repository on disk.
+     */
     private List<String> repositories = new ArrayList<>();
     
     /**
-     * 
+     * Default constructor.
      */
     public Config() {
 
     }
 
     /**
+     * Returns an immuatble collection representing the {@code repositories} list.
      * 
-     * @return
+     * @return The immuatble collection representing the {@code repositories} list.
      */
     public List<String> getRepositories() {
-        return this.repositories;
+        return Collections.unmodifiableList(this.repositories);
     }
 
     /**
-     * 
+     * Sets the 
      * @param repositories
      */
     public void setRepositories(final ArrayList<String> repositories) {
