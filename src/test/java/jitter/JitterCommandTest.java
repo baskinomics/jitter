48a634b536af2b0667cc8eed6c0b9236888a8a11
Sean Baskin <seanbaskin@gmail.com>
2019-05-27 11:17:06 -0400

    Added initial tests

--- src/test/java/jitter/JitterCommandTest.java
+++ src/test/java/jitter/JitterCommandTest.java
@@ -1,26 +1,25 @@
 package jitter;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
+import jitter.domain.model.Config;
 import org.junit.jupiter.api.Test;
-
-import io.micronaut.configuration.picocli.PicocliRunner;
-import io.micronaut.context.ApplicationContext;
-import io.micronaut.context.env.Environment;
 
 public class JitterCommandTest {
 
-     @Test
-     public void testWithCommandLineOption() {
-         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-         System.setOut(new PrintStream(baos));
+    /**
+     * Tests the instantiation of a {@link Config} instance.
+     */
+    @Test
+    public void testCreateConfig() {
+        final var config = new Config();
+        assert config.getRepositories().isEmpty();
+    }
 
-         try (ApplicationContext ctx = ApplicationContext.run(Environment.CLI, Environment.TEST)) {
-             String[] args = new String[] { "-v" };
-             PicocliRunner.run(JitterCommand.class, ctx, args);
-
-         }
-     }
+    /**
+     * Tests the instantiation of a {@link Config} instance from a file.
+     */
+    @Test
+    public void testLoadConfigFromFile() {
+        assert false;
+    }
 
 }
